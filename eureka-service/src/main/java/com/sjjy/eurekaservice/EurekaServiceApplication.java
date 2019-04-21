96d6286439403c78fe6992e2f67a720ec0f23446
chansanjie03 <1299828383@qq.com>
2019-04-21 22:27:28 +0800

    3

--- eureka-service/src/main/java/com/sjjy/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/sjjy/eurekaservice/EurekaServiceApplication.java
@@ -2,12 +2,15 @@ package com.sjjy.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class EurekaServiceApplication {
+public class EurekaServiceApplication{
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServiceApplication.class, args);
+        new SpringApplicationBuilder(EurekaServiceApplication.class)
+                .run(args);
     }
-
 }
